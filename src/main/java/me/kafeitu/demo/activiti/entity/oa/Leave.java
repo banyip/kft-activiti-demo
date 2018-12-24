1042d3b3fa51631bb1e9aa24424c9a79e4757868
yeyifeng <banyip@gmail.com>
2018-12-24 14:53:49 +0800

    relavive added

--- src/main/java/me/kafeitu/demo/activiti/entity/oa/Leave.java
+++ src/main/java/me/kafeitu/demo/activiti/entity/oa/Leave.java
@@ -12,7 +12,8 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Map;
-
+import java.util.Set;
+import java.util.HashSet;
 /**
  * Entity: Leave
  *
--- src/main/java/me/kafeitu/demo/activiti/entity/oa/Relative.java
+++ src/main/java/me/kafeitu/demo/activiti/entity/oa/Relative.java
@@ -11,7 +11,8 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Map;
-
+import java.util.Set;
+import java.util.HashSet;
 /**
  * Entity: Relative
  *
