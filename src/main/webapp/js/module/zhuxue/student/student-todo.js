7cb054220fc4a1de9f17abc6b968c6c541dc146b
yeyifeng <banyip@gmail.com>
2019-01-18 14:12:31 +0800

    photo

--- src/main/webapp/js/module/zhuxue/student/student-todo.js
+++ src/main/webapp/js/module/zhuxue/student/student-todo.js
@@ -148,7 +148,7 @@ function loadPartlyDetailWithTaskVars(leaveId,  callback) {
                 for(var i=0;i<v.length;i++)
                 {   
                     for(let key in v[i])
-                        $('input[name=relatives\\['+i+'\\]_' + key+'.studentEdit').val(eval('v['+i+'].'+key));   
+                        $('input[name=relatives\\['+i+'\\]_' + key+'].studentEdit').val(eval('v['+i+'].'+key));   
                 }
             }
             else if (k == 'audits') 
@@ -161,12 +161,12 @@ function loadPartlyDetailWithTaskVars(leaveId,  callback) {
                             for(var j=0;j<eval('v['+i+'].'+key).length;j++)
                                 {
                                     for(let photokey in eval('v['+i+'].'+key))
-                                        $('a[name=audits\\['+i+'\\]_'+key+'\\['+j+'\\]_' + photokey+'.studentEdit').attr('href',ctx+'showPic/'+eval('v['+i+'].'+key+'['+j+']'+photokey));
+                                        $('a[name=audits\\['+i+'\\]_'+key+'\\['+j+'\\]_' + photokey+'].studentEdit').attr('href',ctx+'showPic/'+eval('v['+i+'].'+key+'['+j+']'+photokey));
                                     //add line
                                 }   
                         
                         else
-                        $('input[name=audits\\['+i+'\\]_' + key+'.studentEdit').val(eval('v['+i+'].'+key));   
+                        $('input[name=audits\\['+i+'\\]_' + key+'].studentEdit').val(eval('v['+i+'].'+key));   
                 
                     }
                 }
