70bc28de56b066394b6b9280e4d8aa785c96a58b
yeyifeng <banyip@gmail.com>
2019-01-23 14:54:30 +0800

    photodate

--- src/main/webapp/js/module/zhuxue/student/student-todo.js
+++ src/main/webapp/js/module/zhuxue/student/student-todo.js
@@ -229,7 +229,7 @@ function loadPartlyDetailWithTaskVars(whichpage,leaveId,  callback) {
                                         insertauditphotos(whichpage,'audits\\['+i+'\\]_'+'auditphotosa');
                                     for(let photokey in eval('v['+i+'].'+key+'['+j+']'))
                                     {
-                                        if(photokey.indexOf("photoDate")>0)
+                                        if(photokey.indexOf("photoDate")>=0)
                                             myparent.find('#audits\\['+i+'\\]_'+key+'\\['+j+'\\]_' + photokey).val(eval('v['+i+'].'+key+'['+j+'].'+photokey));
                                         else{
                                             var filenamesstr = eval('v['+i+'].'+key+'['+j+'].'+photokey);
