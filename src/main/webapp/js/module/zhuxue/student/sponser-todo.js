621eb2d952b38b8edd06b4144db9769e0b2c8ff4
yeyifeng <banyip@gmail.com>
2019-01-25 15:52:51 +0800

    sponser

--- src/main/webapp/js/module/zhuxue/student/sponser-todo.js
+++ src/main/webapp/js/module/zhuxue/student/sponser-todo.js
@@ -305,7 +305,7 @@ function loadPartlyDetailWithTaskVars(whichpage,leaveId,  callback) {
 }
 
 
-function submitbtnclick(parentsting,variables)
+function submitbtnclick(parentsting)
 {
 	myparent=$('#'+parentstring);
 	var variables = [];
