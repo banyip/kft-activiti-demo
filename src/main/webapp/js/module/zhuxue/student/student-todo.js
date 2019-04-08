0a993bcedcaa63d332d2333667def6584a3ba3f0
yeyifeng <banyip@gmail.com>
2019-04-08 14:19:29 +0800

    student_photo

--- src/main/webapp/js/module/zhuxue/student/student-todo.js
+++ src/main/webapp/js/module/zhuxue/student/student-todo.js
@@ -336,7 +336,7 @@ function loadPartlyDetailWithTaskVars(whichpage,leaveId,  callback) {
                 {
                     //myparent.find("a[name="+k+"].studentPhoto").attr('href',ctx+'/zhuxue/student/showPic/'+v);                
                     //myparent.find("a[name="+k+"].studentPhoto").text("显示图片");
-                    showMultiplePics(myparent,filename,'a[id=student_"'+k+'"].studentPhoto');
+                    showMultiplePics(myparent,filename,'a[id="student_'+k+'"]');
                 }
             }
              else if (k.substr(0,2) == 'if') 
