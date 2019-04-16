012b9eaeba1367f5ab4c46c372b02dbd86eb1f33
yeyifeng <banyip@gmail.com>
2019-04-16 15:52:30 +0800

    goon

--- src/main/webapp/js/module/zhuxue/student/sponser-todo.js
+++ src/main/webapp/js/module/zhuxue/student/sponser-todo.js
@@ -3,32 +3,32 @@ var innerhtmls=new Map();
 
 
 var TempArr=[];//存储option
- 
+
 $(function(){
     /*先将数据存入数组*/
-    $("#transfers[0]_studentId option").each(function(index, el) {
+    $("#typenum option").each(function(index, el) {
         TempArr[index] = $(this).text();
     });
     $(document).bind('click', function(e) {  
         var e = e || window.event; //浏览器兼容性   
         var elem = e.target || e.srcElement;  
         while (elem) { //循环判断至跟节点，防止点击的是div子元素   
-            if (elem.id && (elem.id == 'typenum' || elem.id == "transfers[0]_studentId")) {  
+            if (elem.id && (elem.id == 'typenum' || elem.id == "makeupCo")) {  
                 return;  
             }  
             elem = elem.parentNode;  
         }  
-        $('#transfers[0]_studentId').css('display', 'none'); //点击的不是div或其子元素   
+        $('#typenum').css('display', 'none'); //点击的不是div或其子元素   
     });  
 })
  
 function changeF(this_) {
     $(this_).prev("input").val($(this_).find("option:selected").text());
-    $("#transfers[0]_studentId").css({"display":"none"});
+    $("#typenum").css({"display":"none"});
 }
 function setfocus(this_){
-    $("#transfers[0]_studentId").css({"display":""});
-    var select = $("#transfers[0]_studentId");
+    $("#typenum").css({"display":""});
+    var select = $("#typenum");
     for(i=0;i<TempArr.length;i++){
         var option = $("<option></option>").text(TempArr[i]);
         select.append(option);
@@ -36,7 +36,7 @@ function setfocus(this_){
 }
  
 function setinput(this_){
-    var select = $("#transfers[0]_studentId");
+    var select = $("#typenum");
     select.html("");
     for(i=0;i<TempArr.length;i++){
         //若找到以txt的内容开头的，添option
@@ -45,7 +45,7 @@ function setinput(this_){
             select.append(option);
         }
     }
-}          
+}
 
 
 /**
