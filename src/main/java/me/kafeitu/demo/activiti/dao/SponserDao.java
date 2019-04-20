957f1284533c0fa3534bb819141453253e12e338
yeyifeng <yeyifeng@fsyeyifeng2>
2019-04-21 07:54:46 +0800

    goon

--- src/main/java/me/kafeitu/demo/activiti/dao/SponserDao.java
+++ src/main/java/me/kafeitu/demo/activiti/dao/SponserDao.java
@@ -20,7 +20,7 @@ public interface SponserDao extends CrudRepository<Sponser, Long> {
 	@Query(value = "delete from zhuxue_student_transfers where transfers in (select id from zhuxue_transfer where sponser_id = ?1)",nativeQuery=true)
 	public void deleteTransferForeignKey(Long sponser_id);
 	
-	@Query(value = "select a from sponser a where sponserNo like '%?1%' or name like '%?1%’")
+	@Query(value = "select a from sponser a where sponserNo like '%?1%' or name like '%?1%‘")
 	public Iterable<Sponser> search(String queryString);
 	Iterable<Sponser> findBySponserNoOrderByIdAsc(String ponserNo);
 }
