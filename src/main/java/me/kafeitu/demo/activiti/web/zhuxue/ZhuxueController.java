8c67a0abbab4998a6018d0acfd46d020a1720de9
yeyifeng <yeyifeng@fsyeyifeng2>
2019-04-21 18:47:19 +0800

    use response

--- src/main/java/me/kafeitu/demo/activiti/web/zhuxue/ZhuxueController.java
+++ src/main/java/me/kafeitu/demo/activiti/web/zhuxue/ZhuxueController.java
@@ -164,16 +164,17 @@ public class ZhuxueController {
      * 获取提示
      *
      * @param student
+     * @throws IOException 
      */
     @RequestMapping(value = "query/sponsertips")
-    public String sponserTips(HttpSession session, HttpServletRequest request) {
+    public void sponserTips(HttpSession session, HttpServletRequest request,HttpServletResponse response) throws IOException {
         String userId = UserUtil.getUserFromSession(session).getId();
     	String result="";
     	String queryString = request.getParameter("queryString");
         if(queryString.length()>0)
 			result=sponserManager.getNamesAndSponserNo(queryString);
        	logger.debug("tips:"+result);
-        return result;
+        response.getWriter().write(result);
     }   
     
     
