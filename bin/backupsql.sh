a0873cbb62bfb5f187b49ae95ac5b7f91bd10bc8
banyip@gmail.com <banyip@gmail.com>
2019-04-22 14:47:29 +0800

    add install.sh backupsql.sh restoresql.sh

---
+++ bin/backupsql.sh
@@ -0,0 +1,3 @@
+find /mnt/dav/databasebackup -mtime +15 -name "kaddump*.sql" -exec rm -f {} \;
+filedate=`date +%Y%m%d%H%M%S`
+mysqldump -u kad -p Haoyouying1@# kad1 > /mnt/dav/databasebackup/kaddump$filedate.sql
---
+++ bin/restoresql.sh
@@ -0,0 +1,2 @@
+#First: Create database kad
+mysql -ukad -pHaoyouying1@# -D kad < $1
