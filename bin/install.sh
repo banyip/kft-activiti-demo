402161abf08659c754cb1bec6b029d9a94508ac6
banyip@gmail.com <banyip@gmail.com>
2019-05-09 14:33:58 +0800

    change localhost to 127.0.0.1 and some install things

--- bin/install.sh
+++ bin/install.sh
@@ -1,6 +1,10 @@
 sudo apt-get install -y mysql-server
+systemctl enable mariadb.service
+systemctl start mariadb.service
 sudo mysql -uroot < initial.sql 
+systemctl 
 INSTALL_DIR=`pwd`
+mkdir ../pictures
 sudo mkdir -p /mnt/dav
 sudo apt install -y davfs2
 sudo mount -t davfs http://202.105.18.226:18082/seafdav/ /mnt/dav
