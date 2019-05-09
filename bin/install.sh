efff5f06ff9cd8d3d777c54dfcca66b61078d074
banyip <banyip@gmail.com>
2019-05-09 09:46:23 +0800

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -4,4 +4,4 @@ INSTALL_DIR=`pwd`
 sudo mkdir -p /mnt/dav
 sudo apt install -y davfs2
 sudo mount -t davfs http://202.105.18.226:18082/seafdav/ /mnt/dav
-sudo echo "0 5 * * * $INSTALL_DIR/bin/backupmysql.sh >>  /tmp/tmp.txt >> /var/spool/cron/root
+sudo echo "0 5 * * * $INSTALL_DIR/bin/backupmysql.sh" >>  /tmp/tmp.txt >> /var/spool/cron/root
