436573edbf654ce0ac8c0a9cd45b5292fdf02eea
banyip <banyip@gmail.com>
2019-05-09 14:28:20 +0800

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,12 @@
-sudo apt-get install -y mysql-server maven
+sudo apt-get install -y mysql-server
+systemctl enable mariadb.service
+systemctl start mariadb.service
 sudo mysql -uroot < initial.sql 
+systemctl 
 INSTALL_DIR=`pwd`
+mkdir ../pictures
 sudo mkdir -p /mnt/dav
 sudo apt install -y davfs2
 sudo mount -t davfs http://202.105.18.226:18082/seafdav/ /mnt/dav
-sudo echo "0 5 * * * $INSTALL_DIR/bin/backupmysql.sh" >>  /tmp/tmp.txt >> /var/spool/cron/root
+sudo echo "0 5 * * * $INSTALL_DIR/bin/backupmysql.sh >>  /tmp/tmp.txt >> /var/spool/cron/root
+
